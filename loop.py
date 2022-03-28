248c296e8f0d50371529af8783019cb4f878ed5b
ashis nayak <ashisnayak7105@gmail.com>
2022-03-28 13:04:37 +0530

    loop

---
+++ loop.py
@@ -0,0 +1,18 @@
+#nested for loop
+
+# for x in range(4):
+#     for y in range(3):
+#      print(f'({x},{y})')
+
+#print using loop
+#output:xxxxx
+#xx
+#xxxxx
+#xx
+#xx
+# number=[5,2,5,2,2]
+# for x_count in number:
+#     output=''
+#     for count in range(x_count):
+#         output+='x'
+#     print(output)
